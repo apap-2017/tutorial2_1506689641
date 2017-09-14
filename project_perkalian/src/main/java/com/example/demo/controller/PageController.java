141fb23c15c10179b27423a557fe006ac6ec4c70
Rizkah Shalihah <rizkah.shalihah@office.ui.ac.id>
2017-09-15 01:21:03 +0700

    percobaan commit saja

---
+++ project_perkalian/src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+	@RequestMapping("/perkalian")
+	public String hitungPerkalian (@RequestParam(value="a", defaultValue = "0") int a, @RequestParam(value = "b", defaultValue = "0") int b, Model model) {
+		model.addAttribute("a", a);
+		model.addAttribute("b", b);
+		int hasil = a * b;
+		model.addAttribute("hasil", hasil);
+		return "perkalian";
+	}
+}
