ef0058fbaa5ad61f122ffac774bf0d9f8df9ea3a
Rizkah Shalihah <rizkah.shalihah@office.ui.ac.id>
2017-09-15 01:33:35 +0700

    Lab 2 APAP

--- project_perkalian/src/main/java/com/example/demo/controller/PageController.java
+++ project_perkalian/src/main/java/com/example/demo/controller/PageController.java
@@ -11,7 +11,7 @@ public class PageController {
 	public String hitungPerkalian (@RequestParam(value="a", defaultValue = "0") int a, @RequestParam(value = "b", defaultValue = "0") int b, Model model) {
 		model.addAttribute("a", a);
 		model.addAttribute("b", b);
-		int hasil = a * b;
+		int hasil = a * b; //kali nilai a dan b
 		model.addAttribute("hasil", hasil);
 		return "perkalian";
 	}
